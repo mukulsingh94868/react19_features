77dab80c3eb010b5ec6e0dfccae1b22bf10d5f8d
@Mukul <mukul.singh@zentekinfosoft.com>
2024-09-27 15:03:55 +0530

    use optimistic

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import LoginReact19 from './components/login-react-19'
+import TodoList from './components/todo'
 
 function App() {
   return (
     <>
       <h1>Hello World</h1>
-      <LoginReact19 />
+      {/* <LoginReact19 /> */}
+      <TodoList />
     </>
   )
 }
