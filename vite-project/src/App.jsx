f256cceb7f6e4a6237c91c5eaa2a778b7ebd1009
@Mukul <mukul.singh@zentekinfosoft.com>
2024-09-27 14:48:54 +0530

    useFormStatus hook

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import LoginReact19 from './components/login-react-19'
 
 function App() {
   return (
     <>
       <h1>Hello World</h1>
+      <LoginReact19 />
     </>
   )
 }
